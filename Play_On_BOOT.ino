7460cb136f6bf14ae2bdfb3b36dcd7f03fe52c6b
Nitesh Chavan <76774483+niteshchavan@users.noreply.github.com>
2023-12-05 21:15:01 +0530

    Update Play_On_BOOT.ino

--- Play_On_BOOT.ino
+++ Play_On_BOOT.ino
@@ -2,8 +2,8 @@
 
 #define AX2358_address 0b1001010
 
-int gain = 0b11010000;
-int level = 0b11100000; 
+byte gain = 0b11010000;
+byte level = 0b11100000; 
 
 void setup() {
   Wire.begin();
@@ -24,7 +24,7 @@ void setup() {
   delay(300);
 
   // Transmit Audio to AX2358 chip
-  Wire.beginTransmission(0x4a); 
+  Wire.beginTransmission(AX2358_address); 
   Wire.write(0b11000100);
   Wire.write (gain);
   Wire.write (level);
